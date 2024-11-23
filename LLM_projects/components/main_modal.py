ba6e595be3282058df621bd6a17fcfa8b0d76239
coreyddsc <coreyd.dsc@gmail.com>
2024-11-23 17:25:39 -0500

    update app class data storage across components

--- LLM_projects/components/main_modal.py
+++ LLM_projects/components/main_modal.py
@@ -4,74 +4,53 @@ from dash.exceptions import PreventUpdate  # Import PreventUpdate
 
 import dash_bootstrap_components as dbc
 from tools.gkg_tools import *
-# Set up the GKG operator to use gkg_tools
-gkg = gkg_operator() # create a gkg operator
-# manga = pd.read_csv('.\\LLM_projects\\manga_soup_labeled.csv')
-manga = pd.read_csv('data\\manga_soup_labeled.csv')
-manga = manga[manga['sourcecommonname'].map(manga['sourcecommonname'].value_counts()) > 5]
-manga = manga[manga['sharingimage'].notnull()]
-gkg.get_gkg(data=manga) # stores in gkg.gkg_query as a dataframe
-gkg.parse_urls()
-v2tone = gkg.parse_gkg_field('v2tone')
-print(v2tone['Word Count'])
-print(gkg.gkg_query.columns)
-# gkg.parse_gkg_field('allnames')
-# gkg.vectorize_field(weight='weighted')
-# gkg.get_fields_stats(weight='weighted')
-# gkg.parse_gkg_soup(url=gkg.urls[0],verbose=True)
 
-# Sample images for the grid from manga sharingimage for rows in manga.
-# manga is stored in gkg.gkg_query
-images = [
-    {"src": row["sharingimage"], "alt": row["title"]}
-    for _, row in gkg.gkg_query.iterrows()
-]
+def image_grid(app):
+    image_grid = html.Div(id="image-grid", 
+                        children=[
+                                html.Div(
+                                    className="image-container",  # Container for image and title
+                                    children=[
+                                        html.Img(
+                                            src=image["src"],
+                                            alt=image["alt"],
+                                            className="grid-item",
+                                            id=f"img-{index}",  # Unique ID for each image
+                                        ),
+                                        html.Div(
+                                            className="image-title",  # Title text overlay
+                                            children=image["alt"],  # Use the title text
+                                        ),
+                                    ],
+                                )
+                                for index, image in enumerate(app.images)
+                            ])
+    return image_grid
 
-image_store = dcc.Store(id="images-store", data=images)
-n_clicks_store = dcc.Store(id="n_clicks_store", data=[0] * len(images))
-
-image_grid = html.Div(id="image-grid", 
-                      children=[
-                            html.Div(
-                                className="image-container",  # Container for image and title
-                                children=[
-                                    html.Img(
-                                        src=image["src"],
-                                        alt=image["alt"],
-                                        className="grid-item",
-                                        id=f"img-{index}",  # Unique ID for each image
-                                    ),
-                                    html.Div(
-                                        className="image-title",  # Title text overlay
-                                        children=image["alt"],  # Use the title text
-                                    ),
-                                ],
-                            )
-                            for index, image in enumerate(images)
-                        ])
-
-# Modal for displaying image details
-main_modal = dbc.Modal(
-            [
-                dbc.ModalBody(id="modal-content", children="This is the modal content."),
-                dbc.ModalFooter(
-                    dbc.Button(
-                        "×",  # Close button text
-                        id="close-modal-btn",  # ID for callback
-                        className="btn-close",  # Apply optional CSS
-                        style={"margin-left": "auto", "color": "black", "display":"flex"},  # Inline styling
-                    ), # Show close button
-                ), # Modal footer
-            ],
-            id="image-modal",
-            scrollable=True,  # Allow modal to scroll
-            is_open=False,  # Modal starts closed
-            size="xl",
-            backdrop="static",  # Disable clicking outside of modal to close
-            autoFocus=True,  # Auto focus on modal
-            centered=True,  # Center the modal
-        )
-
+def main_modal(app):
+    # Modal for displaying image details
+    main_modal = dbc.Modal(
+                [
+                    dcc.Store(id="n_clicks_store", data=[0] * len(app.images)),  # Store the number of clicks
+                    dbc.ModalBody(id="modal-content", children="This is the modal content."),
+                    dbc.ModalFooter(
+                        dbc.Button(
+                            "×",  # Close button text
+                            id="close-modal-btn",  # ID for callback
+                            className="btn-close",  # Apply optional CSS
+                            style={"margin-left": "auto", "color": "black", "display":"flex"},  # Inline styling
+                        ), # Show close button
+                    ), # Modal footer
+                ],
+                id="image-modal",
+                scrollable=True,  # Allow modal to scroll
+                is_open=False,  # Modal starts closed
+                size="xl",
+                backdrop="static",  # Disable clicking outside of modal to close
+                autoFocus=True,  # Auto focus on modal
+                centered=True,  # Center the modal
+            )
+    return main_modal
 
 def main_modal_callbacks(app):
     # Callback to toggle or close the modal
@@ -82,7 +61,7 @@ def main_modal_callbacks(app):
             Output("n_clicks_store", "data"),  # Update the stored state
         ],
         [
-            Input(f"img-{index}", "n_clicks") for index in range(len(images))
+            Input(f"img-{index}", "n_clicks") for index in range(len(app.images))
         ] + [Input("close-modal-btn", "n_clicks")],
         [
             State("image-modal", "is_open"),
@@ -99,8 +78,8 @@ def main_modal_callbacks(app):
         print(f"Triggered ID: {triggered_id}")
 
         # Separate Inputs and States
-        n_clicks_new = args[:len(images)]  # Inputs: Current n_clicks for images
-        n_clicks_old = args[len(images) + 2]  # State: Previous n_clicks from Store
+        n_clicks_new = args[:len(app.images)]  # Inputs: Current n_clicks for images
+        n_clicks_old = args[len(app.images) + 2]  # State: Previous n_clicks from Store
 
         # Handle None values
         n_clicks_new = [0 if click is None else click for click in n_clicks_new]
@@ -109,7 +88,7 @@ def main_modal_callbacks(app):
         # Handle Close Button Click
         if triggered_id == "close-modal-btn":
             print("Close button clicked")
-            return False, "", [0] * len(images)
+            return False, "", [0] * len(app.images)
 
         # Handle Image Click
         if triggered_id.startswith("img-"):
@@ -117,16 +96,16 @@ def main_modal_callbacks(app):
             print(f"Image {clicked_index} clicked")
 
             # Use gkg tools to parse the soup of the clicked image url
-            gkg.parse_gkg_soup(url=gkg.urls[clicked_index],verbose=True)
+            app.gkg.parse_gkg_soup(url=app.gkg.urls[clicked_index],verbose=True)
             # Modal Content
             model_content = html.Div([
                 dbc.ModalTitle(
-                    f"{images[clicked_index]['alt']}",
+                    f"{app.images[clicked_index]['alt']}",
                     style={"color": "black", "textAlign": "center", 'font-size': '55px'}
                 ),
-                html.Img(src=images[clicked_index]['src'], style={"maxWidth": "100%"}),
+                html.Img(src=app.images[clicked_index]['src'], style={"maxWidth": "100%"}),
                 # Create a parent container for the paragraphs
-                html.Div([html.P(par) for par in gkg.parsed_paragraphs], style={"margin-top": "20px"})
+                html.Div([html.P(par) for par in app.gkg.parsed_paragraphs], style={"margin-top": "20px"})
             ])
             # Update Modal Content and Open Modal
             return True, model_content, n_clicks_new
--- LLM_projects/news_app.py
+++ LLM_projects/news_app.py
@@ -6,8 +6,27 @@ import dash_bootstrap_components as dbc
 from tools.gkg_tools import * # Import the gkg_operator class for gdelt gkg data processing
 from components.main_modal import * # Import the main_modal_callbacks function and image grid layout
 
+
+# Set up the GKG operator to use gkg_tools
+gkg = gkg_operator() # create a gkg operator
+# manga = pd.read_csv('.\\LLM_projects\\manga_soup_labeled.csv')
+manga = pd.read_csv('data\\manga_soup_labeled.csv')
+manga = manga[manga['sourcecommonname'].map(manga['sourcecommonname'].value_counts()) > 5]
+manga = manga[manga['sharingimage'].notnull()]
+gkg.get_gkg(data=manga) # stores in gkg.gkg_query as a dataframe
+gkg.parse_urls()
+# Set up images sample from manga sharingimage for rows in manga.
+
+
 # app setup
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app.manga = manga.copy()
+app.gkg = gkg
+app.images = [
+    {"src": row["sharingimage"], "alt": row["title"]}
+    for _, row in app.gkg.gkg_query.iterrows()
+]
+
 
 app.layout = html.Div(
     children=[
@@ -15,20 +34,19 @@ app.layout = html.Div(
             "ONE PIECE NEWS DASHBOARD!",
             className="dashboard-title",
         ),
-        # Store the images data to be shared across callbacks
-        image_store,
         # Update the image grid with titles
-        image_grid,
+        image_grid(app),
         # Modal for displaying image details
-        main_modal,
-        # Store the number of clicks for each image for tracking
-        n_clicks_store,
+        main_modal(app),
     ]
 )
 
 # callback for updating the modal content
+print(f"Test to see if main modal stored app.images: {app.images}")
+
 main_modal_callbacks(app)
 
+print(f"Test using app.gkg: {app.gkg.parse_gkg_field('v2tone')}")
 
 # Run the app
 if __name__ == '__main__':
